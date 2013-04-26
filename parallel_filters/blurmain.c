b274290f7680ca3cb0e30eb77bfe484bb59fcc9e
Filip Källström <filip.kalls@hotmail.com>
2013-04-26 09:47:34 +0200

    indenting and intialization of mpi

--- parallel_filters/blurmain.c
+++ parallel_filters/blurmain.c
@@ -2,44 +2,59 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <mpi.h>
 #include "ppmio.h"
 #include "blurfilter.h"
 #include "gaussw.h"
 
 
 int main (int argc, char ** argv) {
-   int radius;
+    int radius;
     int xsize, ysize, colmax;
     pixel src[MAX_PIXELS];
     struct timespec stime, etime;
-#define MAX_RAD 1000
+    #define MAX_RAD 1000
+
+
+    /* MPI INIT*/
+    int ierr = MPI_Init(&argc, &argv);
+    int myId, numberProc;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &numberProc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myId);
 
     double w[MAX_RAD];
 
     /* Take care of the arguments */
 
     if (argc != 4) {
-	fprintf(stderr, "Usage: %s radius infile outfile\n", argv[0]);
-	exit(1);
+        fprintf(stderr, "Usage: %s radius infile outfile\n", argv[0]);
+        MPI_Finalize();
+        exit(1);
     }
+
     radius = atoi(argv[1]);
     if((radius > MAX_RAD) || (radius < 1)) {
-	fprintf(stderr, "Radius (%d) must be greater than zero and less then %d\n", radius, MAX_RAD);
-	exit(1);
+        fprintf(stderr, "Radius (%d) must be greater than zero and less then %d\n", radius, MAX_RAD);
+        MPI_Finalize();
+        exit(1);
     }
 
     /* read file */
-    if(read_ppm (argv[2], &xsize, &ysize, &colmax, (char *) src) != 0)
+    if(read_ppm (argv[2], &xsize, &ysize, &colmax, (char *) src) != 0){
+        MPI_Finalize();
         exit(1);
+    }
 
     if (colmax > 255) {
-	fprintf(stderr, "Too large maximum color-component value\n");
-	exit(1);
+        fprintf(stderr, "Too large maximum color-component value\n");
+        MPI_Finalize();
+        exit(1);
     }
 
     printf("Has read the image, generating coefficients\n");
 
-    /* filter */
+        /* filter */
     get_gauss_weights(radius, w);
 
     printf("Calling filter\n");
@@ -51,14 +66,16 @@ int main (int argc, char ** argv) {
     clock_gettime(CLOCK_REALTIME, &etime);
 
     printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) +
-	   1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
+        1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
 
     /* write result */
     printf("Writing output file\n");
-    
-    if(write_ppm (argv[3], xsize, ysize, (char *)src) != 0)
-      exit(1);
 
+    if(write_ppm (argv[3], xsize, ysize, (char *)src) != 0){
+        MPI_Finalize();
+        exit(1);
+    }
 
+    MPI_Finalize();
     return(0);
 }
