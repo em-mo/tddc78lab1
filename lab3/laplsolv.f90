53e765438945a34f5803f27947688da3b7ca727a
Emil <karlemil.lindberg@gmail.com>
2013-05-13 19:24:36 +0200

    Working lab 3

--- lab3/laplsolv.f90
+++ lab3/laplsolv.f90
@@ -6,16 +6,14 @@ program laplsolv
 ! Modified by Berkant Savas (besav@math.liu.se) April 2006
 !-----------------------------------------------------------------------
   double precision omp_get_wtime
-  integer omp_get_num_threads
-  integer omp_get_thread_num
-  integer, parameter                  :: n=1000, maxiter=1000
+  integer omp_get_num_threads,omp_get_thread_num,numthreads,threadId
+  integer, parameter                  :: n=100, maxiter=1000
   double precision,parameter          :: tol=1.0E-3
   double precision,dimension(0:n+1,0:n+1) :: T
-  double precision,dimension(n)       :: tmp1
-  double precision,dimension(0:n+1)   :: tmp2
-  double precision                    :: error,x
+  double precision,dimension(n)       :: tmp1, tmp2, startTmp, endTmp
+  double precision                    :: error
   real                                :: t1,t0
-  integer                             :: i,j,k, iters
+  integer                             :: j,k,iters,startCol, endCol, numColumns, f
   character(len=20)                   :: str
   
   ! Set boundary conditions and initial values for the unknowns
@@ -24,49 +22,76 @@ program laplsolv
   T(0:n+1 , n+1)   = 1.0D0
   T(n+1   , 0:n+1) = 2.0D0
   
+  iter = 0;
 
   ! Solve the linear system of equations using the Jacobi method
   t0 = omp_get_wtime()
 
-  !$omp parallel private(i, j, k, tmp1, error, tmp2) shared(iter)
+  !$omp parallel private(j, k, tmp1, tmp2, startCol, endCol, numthreads, threadId, numColumns, startTmp, endTmp, f) shared(error)
 
   numthreads = omp_get_num_threads()
   threadId = omp_get_thread_num()
 
-  numcolumns = n/numthreads
-  restCols = mod(numcolumns, numthreads)
-
-  if (threadId < restcols) then
-     numcolumns = numcolumns + 1
+  if (mod(n, numthreads) == 0) then
+    numColumns = n / numthreads
+    startCol = numColumns * threadId
+    endCol = startCol + numColumns + 1
+  else
+    numColumns = ceiling(dble(n) / numthreads)
+    if (threadId == numthreads - 1) then
+      startCol = numColumns * (numthreads - 1)
+      endCol = n + 1
+    else
+      startCol = numColumns * threadId
+      endCol = startCol + numColumns + 1
+    end if
   end if
 
-  startColumnIndex = numColumns * thread_id + min(restCols, threadId)
-  lastColumnIndex = startColumnIndex + numColumns
+  f = 1
 
   do k=1,maxiter
-     tmp1=T(1:n,0)
-     error=0.0D0
-     
-     !$omp do     
-     do j=1,n
-        
-                
+    tmp1=T(1:n,startCol)
+    endTmp = T(1:n, endCol)
+
+    !$omp barrier
+    error=0.0D0
+
+    !$omp do reduction(max:error)
+    do j=1,n
+      tmp2=T(1:n,j)
+      
+      if (f == 1) then
+        f = 2
+        write(*,*) "Id ", threadId, " start ", j, " col ", startCol
+      end if
+
+      if (j == endCol - 1) then
+        T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+endTmp+tmp1)/4.0D0
+      else        
         T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+T(1:n,j+1)+tmp1)/4.0D0
-     
-        !$omp critical
-        error=max(error,maxval(abs(tmp2(1:n)-T(1:n,j))))
-        !$omp end critical
+      end if
+   
+      error=max(error,maxval(abs(tmp2-T(1:n,j))))
 
-        tmp1=tmp2(1:n)
+      tmp1=tmp2
+    end do
+    !$omp end do
 
-     end do
-     !$omp end do
-     if (error<tol) then
-        exit
-     end if
-     
+    if (f == 2) then
+        f = 0
+        write(*,*) "Id ", threadId, " end   ", j, " col ", endCol
+      end if
+
+    if (error<tol) then
+      exit
+    end if
   end do
-  iter = k
+
+  !$omp critical
+  if (iter < k) then
+    iter = k
+  end if
+  !$omp end critical
   !$omp end parallel
 
   t1 = omp_get_wtime()
